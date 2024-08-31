2e29d98591db25ea23cf6564b79ba569d19ea110
Jeremiah Swank <jeremiah.swank@atlasschool.com>
2024-08-31 12:49:51 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="h-full flex flex-col justify-between p-8 min-h-screen">
+      <p>Music player goes here</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
